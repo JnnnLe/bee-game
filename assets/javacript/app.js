d62d5f71edd09f26bff3d662b5e0e2c062733cd2
Jennifer Le <j.le@Jennifers-MacBook-Air.local>
2018-01-28 16:20:34 -0800

    Question + answers

--- assets/javacript/app.js
+++ assets/javacript/app.js
@@ -1,7 +1,23 @@
 //vars
 
 //questions
-
+var questions = [{
+  question: "How do bees communicate with one another?",
+  selection: ["Touching antennas", "They don't", "By dancing", "Slack messenger" ],
+  answer: 2
+}, {
+  question: "How many miles must a bee fly in order to produce 1lb of honey?",
+  selection: ["90,000 miles", "5,000 miles", "50,000 miles", "105 miles"],
+  answer: 0
+}, {
+  question: "How long does a Queen bee live before she dies?",
+  selection: ["12 months", "2 years", "5 years", "She's a bad B, you can't kill her." ]
+  answer: 2
+}, {
+  question: "How are bees useful in our modern world?"
+  selection: ["Pollination", "Produces honey", "Produces natural fruit sugars, fructose and glucose.", "All of the above"],
+  answer: 3
+}];
 
 //
 document.ready(function() {

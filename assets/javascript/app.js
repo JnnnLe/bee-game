e8545e0ee67a24bd1d30b63b0b1c610c4d9319aa
Jennifer Le <j.le@Jennifers-MacBook-Air.local>
2018-03-02 20:38:02 -0800

    Styled Q + A

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,32 @@
-//vars
-var currentQuestion = 0;
-var correctAnswers = 0;
-var play = false;
+// $(document).ready(function() {
+  function userSubmission() {
+    var perfectScore = 4;
+    var userScore = 0;
 
-//questions
-var questions = [{
-  question: "How do bees communicate with one another?",
-  selection: ["Touching antennas.", "Male bees fight one another.", "By dancing.", "Slack messenger." ],
-  answer: 2
-}, {
-  question: "How many miles must a bee fly in order to produce 1lb of honey?",
-  selection: ["90,000 miles", "5,000 miles", "50,000 miles", "105 miles"],
-  answer: 0
-}, {
-  question: "How long does a Queen bee live before she dies?",
-  selection: ["12 months", "2 years", "5 years", "She's a bad B, you can't kill her." ],
-  answer: 2
-}, {
-  question: "How are bees useful in our modern world?",
-  selection: ["Pollination", "Produces honey", "Produces natural fruit sugars, fructose and glucose.", "All of the above"],
-  answer: 3
-}];
+    var q1 = document.forms['triviaQuestions']['q1'].value;
+    var q2 = document.forms['triviaQuestions']['q2'].value;
+    var q3 = document.forms['triviaQuestions']['q3'].value;
+    var q4 = document.forms['triviaQuestions']['q4'].value;
 
+    var answers = ['b', 'b', 'a', 'd'];
 
-$(document).ready(function() {
+    for (var i = 1; i <= perfectScore; i++) {
+      if (eval('q' + i) == answers[i - 1]) {
+        userScore++;
+      };
+    };
+    // answers.forEach(function(correctAnswer, i) {
+    //   if (eval("q"[i + 1] == answers[i + 1])) {
+    //     userScore++;
+    //   }
+    // })
 
-displayQA();
+    var results = document.getElementById("final-score");
 
-//display Question
-function displayQA() {
-  //iterate through each answer and display each in new div
-  for (var i = 0; i < 4; i++) {
-  $("#currentQuestion").html(questions[currentQuestion].question);
-  // $("#answers" + questions[currentQuestion].selection[i] + "name='dynradio'/>").append("#fun");
-  $("#answers").append("<button type='button'>" + questions[currentQuestion].selection[i] "</button>");
+    results.innerHTML = '<h1>You scored <span>' + userScore + '</span> out of <span>' + perfectScore + '</span></h1>';
+    
+    return false;
   }
-};
-
-
-
-
-
-
 
 //end of document 
-});
+// });
